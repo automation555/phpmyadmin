d7aaae3ea2c9ee0ff0198a438a93d288aa2c4b87
rajashree patil <rajashree.patil@embold.io>
2021-06-17 18:27:48 +0530

    commitMessage : Fix #16487 - Add paging to Routines page

--- libraries/classes/Controllers/Database/RoutinesController.php
+++ libraries/classes/Controllers/Database/RoutinesController.php
@@ -9,11 +9,11 @@ use PhpMyAdmin\Core;
 use PhpMyAdmin\Database\Routines;
 use PhpMyAdmin\DatabaseInterface;
 use PhpMyAdmin\DbTableExists;
+use PhpMyAdmin\Html\Generator;
 use PhpMyAdmin\Response;
 use PhpMyAdmin\Template;
 use PhpMyAdmin\Url;
 use PhpMyAdmin\Util;
-
 use function in_array;
 use function strlen;
 
@@ -44,13 +44,21 @@ class RoutinesController extends AbstractController
     {
         global $db, $table, $tables, $num_tables, $total_num_tables, $sub_part;
         global $tooltip_truename, $tooltip_aliasname, $pos;
-        global $errors, $errorUrl, $urlParams, $cfg;
-
-        $this->addScriptFiles(['database/routines.js']);
+        global $errors, $PMA_Theme, $text_dir, $err_url, $url_params, $cfg;
 
         $type = $_REQUEST['type'] ?? null;
 
         $this->checkUserPrivileges->getPrivileges();
+
+        [
+            $tables,
+            $num_tables,
+            $total_num_tables,
+            $sub_part,,,
+            $tooltip_truename,
+            $tooltip_aliasname,
+            $pos,
+        ] = Util::getDbInfo($db, $sub_part ?? '');
 
         if (! $this->response->isAjax()) {
             /**
@@ -59,9 +67,9 @@ class RoutinesController extends AbstractController
             if (! empty($table) && in_array($table, $this->dbi->getTables($db))) {
                 Util::checkParameters(['db', 'table']);
 
-                $urlParams = ['db' => $db, 'table' => $table];
-                $errorUrl = Util::getScriptNameForOption($cfg['DefaultTabTable'], 'table');
-                $errorUrl .= Url::getCommon($urlParams, '&');
+                $url_params = ['db' => $db, 'table' => $table];
+                $err_url = Util::getScriptNameForOption($cfg['DefaultTabTable'], 'table');
+                $err_url .= Url::getCommon($url_params, '&');
 
                 DbTableExists::check();
             } else {
@@ -69,22 +77,12 @@ class RoutinesController extends AbstractController
 
                 Util::checkParameters(['db']);
 
-                $errorUrl = Util::getScriptNameForOption($cfg['DefaultTabDatabase'], 'database');
-                $errorUrl .= Url::getCommon(['db' => $db], '&');
+                $err_url = Util::getScriptNameForOption($cfg['DefaultTabDatabase'], 'database');
+                $err_url .= Url::getCommon(['db' => $db], '&');
 
                 if (! $this->hasDatabase()) {
                     return;
                 }
-
-                [
-                    $tables,
-                    $num_tables,
-                    $total_num_tables,
-                    $sub_part,,,
-                    $tooltip_truename,
-                    $tooltip_aliasname,
-                    $pos,
-                ] = Util::getDbInfo($db, $sub_part ?? '');
             }
         } elseif (strlen($db) > 0) {
             $this->dbi->selectDb($db);
@@ -107,6 +105,28 @@ class RoutinesController extends AbstractController
         }
 
         $items = $this->dbi->getRoutines($db, $type);
+        $totalNumRoutines = count($items);
+        $pageSize = $cfg['MaxRoutineList'];
+
+        // Checks if there are any routines to be shown on current page.
+        // If there are no routines, the user is redirected to the last page
+        // having any.
+        if ($totalNumRoutines > 0 && $pos >= $totalNumRoutines) {
+            $redirParams = [
+                'db' => $this->db,
+                'pos' => max(0, $totalNumRoutines - $pageSize),
+                'reload' => 1,
+            ];
+            if ($this->response->isAjax()) {
+                $redirParams['ajax_request'] = 'true';
+                $redirParams['ajax_page_request'] = 'true';
+            }
+            $uri = './index.php?route=/database/routines' . Url::getCommonRaw($redirParams, '&');
+            Core::sendHeaderLocation($uri);
+        }
+
+        $items = array_slice($items, $pos, $pageSize);
+
         $isAjax = $this->response->isAjax() && empty($_REQUEST['ajax_page_request']);
 
         $rows = '';
@@ -114,12 +134,28 @@ class RoutinesController extends AbstractController
             $rows .= $routines->getRow($item, $isAjax ? 'ajaxInsert hide' : '');
         }
 
+        $urlParams = [
+            'pos' => $pos,
+            'db' => $db,
+        ];
+
+        $listNavigator = Generator::getListNavigator(
+                $totalNumRoutines,
+                $pos,
+                $urlParams,
+                Url::getFromRoute('/database/routines'),
+                'frame_content',
+                $pageSize
+            );
+
         $this->render('database/routines/index', [
             'db' => $db,
             'table' => $table,
             'items' => $items,
             'rows' => $rows,
+            'select_all_arrow_src' => $PMA_Theme->getImgPath() . 'arrow_' . $text_dir . '.png',
             'has_privilege' => Util::currentUserHasPrivilege('CREATE ROUTINE', $db, $table),
+            'list_navigator_html' => $listNavigator,
         ]);
     }
 }
